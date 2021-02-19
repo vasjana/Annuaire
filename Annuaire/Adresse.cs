eb282116c8aec7b18115aa3e4b4640e9484c7096
Vasjana Spaho <vasjanaspaho@outlook.fr>
2021-02-19 14:00:59 +0100

    Initialisation commit projet Annuaire.

---
+++ Annuaire/Adresse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Annuaire
+{
+    public class Adresse
+    {
+        #region Attributs
+
+        private int _NumeroDeLaRue { get; set; }
+        private string _NomDeLaRue { get; set; }
+        private int _CodePostal { get; set; }
+        private string _Ville { get; set; }
+        private string _Pays { get; set; }
+
+        #endregion
+
+
+        #region Méthodes
+
+        public void AfficherAdress(int numeroderue, string nomdelarue, int codepostal, string ville, string pays)
+        {
+            _NumeroDeLaRue = numeroderue;
+            _NomDeLaRue = nomdelarue;
+            _CodePostal = codepostal;
+            _Ville = ville;
+            _Pays = pays;
+
+            Console.WriteLine("L'adresse est " + _NumeroDeLaRue + " "  + _NomDeLaRue + " , "  + _CodePostal + " ,"
+                + _Ville + ", "  + _Pays + ".");
+           
+        }
+
+        #endregion
+
+    }
+}
---
+++ Annuaire/Utilisateur.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Annuaire
+{
+    public class Utilisateur
+    {
+        #region Attributs
+
+        private string _Nom {get;set;}
+        private string _Prenom { get; set; }
+        private int _NumeroDeTelephone{ get; set; }
+        private string _Login { get; set; }
+
+        #endregion
+
+        #region Méthodes
+
+        public void AfficherInformationUtilisateur(string nom, string prenom, int tel, string login)
+        {
+            _Nom = nom;
+            _Prenom = prenom;
+            _NumeroDeTelephone = tel;
+            _Login = login;
+
+            Console.WriteLine("Le nom d'utilisateur est " + _Nom);
+            Console.WriteLine("Le prenom d'utilisateur est " + _Prenom);
+            Console.WriteLine("Le numéro de téléphone d'utilisateur est " + _NumeroDeTelephone);
+            Console.WriteLine("Login: " + _Login);
+
+        }
+
+        #endregion
+    }
+}
