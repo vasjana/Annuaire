38ad9ba9c42e4f50379809e4db0b4bc02a8fcec3
Vasjana Spaho <vasjanaspaho@outlook.fr>
2021-03-02 12:40:18 +0100

    Lire un fichier excel

--- Annuaire/Program.cs
+++ Annuaire/Program.cs
@@ -4,17 +4,18 @@ using OfficeOpenXml;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using static System.Net.Mime.MediaTypeNames;
 namespace Annuaire
 {
     class Program
     {
-        
+
 
         static void Main(string[] args)
         {
             Utilisateur utilisateur = new Utilisateur();
-            utilisateur = utilisateur.CreerUtilisateur();
+            //utilisateur = utilisateur.CreerUtilisateur();
 
 
 
@@ -28,67 +29,86 @@ namespace Annuaire
                 var excelWorksheet = excel.Workbook.Worksheets["Worksheet1"];
 
 
-                int countRows = excelWorksheet.Dimension.End.Row;
 
-                // Add Row 3
-                //Add some items...
+                //int countRows = excelWorksheet.Dimension.End.Row;
 
-                excelWorksheet.Cells[countRows+1, 1].Value = utilisateur.Login;
-                excelWorksheet.Cells[countRows+1, 2].Value = utilisateur.Nom;
-                excelWorksheet.Cells[countRows+1, 3].Value = utilisateur.Prenom;
-                excelWorksheet.Cells[countRows+1, 4].Value = utilisateur.Tel;
-                excelWorksheet.Cells[countRows+1, 5].Value = utilisateur.Adresse.NumeroDeRue;
-                excelWorksheet.Cells[countRows+1, 6].Value = utilisateur.Adresse.NomDeRue;
-                excelWorksheet.Cells[countRows+1, 7].Value = utilisateur.Adresse.CodePostal;
-                excelWorksheet.Cells[countRows+1, 8].Value = utilisateur.Adresse.Ville;
-                excelWorksheet.Cells[countRows+1, 9].Value = utilisateur.Adresse.Pays;
+                //// Add Row 3
+                ////Add some items...
 
+                //excelWorksheet.Cells[countRows + 1, 1].Value = utilisateur.Login;
+                //excelWorksheet.Cells[countRows + 1, 2].Value = utilisateur.Nom;
+                //excelWorksheet.Cells[countRows + 1, 3].Value = utilisateur.Prenom;
+                //excelWorksheet.Cells[countRows + 1, 4].Value = utilisateur.Tel;
+                //excelWorksheet.Cells[countRows + 1, 5].Value = utilisateur.Adresse.NumeroDeRue;
+                //excelWorksheet.Cells[countRows + 1, 6].Value = utilisateur.Adresse.NomDeRue;
+                //excelWorksheet.Cells[countRows + 1, 7].Value = utilisateur.Adresse.CodePostal;
+                //excelWorksheet.Cells[countRows + 1, 8].Value = utilisateur.Adresse.Ville;
+                //excelWorksheet.Cells[countRows + 1, 9].Value = utilisateur.Adresse.Pays;
 
-
-                //List<string[]> headerRow = new List<string[]>()
-                // {
-                //    new string[] { "LOGIN", "NOM", "PRENOM", "TELEPHONE", "NUM_RUE" , "NOM_RUE", "CODE_POSTAL", "VILLE", "PAYS" }
-                // };
-
-
-                FileInfo excelFile = new FileInfo(@"D:\csharp\Annuaire\Output\Annuaire.xlsx");
-                excel.SaveAs(excelFile);
-
-                //// Determine the header range (e.g. A1:E1)
-
-                //    string headerRange = "A1:" + Char.ConvertFromUtf32(headerRow[0].Length + 64) + "1";
-                //    // Popular header row data
-                //    excelWorksheet.Cells[headerRange].LoadFromArrays(headerRow);
-
-                //    List<string[]> cellData = new List<string[]>()
-                //    {
-                //       new string[] {utilisateur.Login, utilisateur.Nom, utilisateur.Prenom, utilisateur.Tel, utilisateur.Adresse.NumeroDeRue,
-                //       utilisateur.Adresse.NomDeRue, utilisateur.Adresse.CodePostal, utilisateur.Adresse.Ville, utilisateur.Adresse.Pays}
-
-
-                //};
-                //excelWorksheet.Cells[2, 1].LoadFromArrays(cellData);
-
-                // Find the last real row
-                //nInLastRow = oSheet.Cells.Find("*", System.Reflection.Missing.Value,
-                //System.Reflection.Missing.Value, System.Reflection.Missing.Value, Excel.XlSearchOrder.xlByRows, Excel.XlSearchDirection.xlPrevious, false, System.Reflection.Missing.Value, System.Reflection.Missing.Value).Row;
-
-
-                //FileInfo excelFile = new FileInfo(@"D:\csharp\Annuaire\Output\Annuaire.xlsx");
-                //excel.SaveAs(excelFile);
-
-                //bool isExcelInstalled = Type.GetTypeFromProgID("Excel.Application") != null ? true : false;
-                //if (isExcelInstalled)
+                //try
                 //{
-                //    System.Diagnostics.Process.Start(excelFile.ToString());
+                //    utilisateur.Tel;
+
                 //}
 
 
+                int colCount = excelWorksheet.Dimension.End.Column;  //get Column Count
+                int rowCount = excelWorksheet.Dimension.End.Row;     //get row count
+
+                Console.WriteLine(" Cet tableau a " + rowCount + " lignes.");
+
+                //for (int row = 1; row <= rowCount - 1; row++)
+                //{
+                //Console.WriteLine("L'utilisateur : " + excelWorksheet.Cells[row + 1, 1].Value + ", son nom est " + excelWorksheet.Cells[row + 1, 2].Value
+                //    + " " + excelWorksheet.Cells[row + 1, 3].Value + ", son numéro de téléphone est " + excelWorksheet.Cells[row + 1, 4].Value
+                //    + ", son adresse est " + excelWorksheet.Cells[row + 1, 5].Value + "," + excelWorksheet.Cells[row + 1, 6].Value
+                //    + "," + excelWorksheet.Cells[row + 1, 7].Value + "," + excelWorksheet.Cells[row + 1, 8].Value + "," + excelWorksheet.Cells[row + 1, 9].Value
+                //    + ".");
+
+                ////Console.WriteLine("L'utilisateur : " + excelWorksheet.Cells[row + 1, col].Value + ", son nom est " + excelWorksheet.Cells[row + 1, col].Value
+                ////   + " " + excelWorksheet.Cells[row + 1, col].Value + ", son numéro de téléphone est " + excelWorksheet.Cells[row + 1, col].Value
+                ////   + ", son adresse est " + excelWorksheet.Cells[row + 1, col].Value + "," + excelWorksheet.Cells[row + 1, col].Value
+                ////   + "," + excelWorksheet.Cells[row + 1, col].Value + "," + excelWorksheet.Cells[row + 1, col].Value + "," + excelWorksheet.Cells[row + 1, col].Value
+                ////   + ".");
+
+                ////for (int col = 1; col <= colCount; col++)
+                ////    {
+
+                ////        Console.WriteLine("Le " + (row) + " utilisateur est " + excelWorksheet.Cells[row + 1, col].Value.ToString());
+                ////        Console.ReadLine();
+                ////    }
+
+                //}
+
+                //Console.ReadLine();
 
 
+               
 
+
+                for (int i = 1; i <= rowCount; i++)
+                {
+                    for (int j = 1; j <= colCount; j++)
+                    {
+                        //new line
+                        if (j == 1)
+                            Console.Write("\r\n");
+
+                        //write the value to the console
+                        if (excelWorksheet.Cells[i, j] != null && excelWorksheet.Cells[i, j].Value != null)
+                            Console.Write(excelWorksheet.Cells[i, j].Value.ToString() + "\t");
+
+                        //add useful things here!   
+                    }
+                    Console.ReadLine();
+                }
+                FileInfo excelFile = new FileInfo(@"D:\csharp\Annuaire\Output\Annuaire.xlsx");
+                excel.SaveAs(excelFile);
 
             }
+
+
+
 
 
 
@@ -102,5 +122,13 @@ namespace Annuaire
 
 
 
+
+
+
+
     }
+
+
+
+
 }
